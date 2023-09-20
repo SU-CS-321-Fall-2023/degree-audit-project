1073fa8d563a6151d7a1b0bab51f3562a6858f63
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-09-20 11:40:33 -0400

    Created front-end/
    11:40 AM, 09/20/2023 - Alexander Garofalo
    
    Created the front-end/ directory, index.html, style.css, and header and footer components.

---
+++ front-end/src/components/footer.js
@@ -0,0 +1,62 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="footer" aria-label="Site footer">
+            <div class="footer__container">
+                <div class="footer__container__row">
+                    <div class="footer__left_column">
+                        <div class="The_Auditors_Logo__container">
+                            <a href="/front-end/src/index.html" class="footer__logo" id="footer__logo" aria-label="The Auditors' Logo">
+                                <img src="" alt="The Auditors">
+                            </a>
+                        </div>
+                        <div class="footer__social--desktop">
+                            <ul class="footer__social__list">
+                                <li class="footer__social__item">
+                                    <a href="" class="footer__social__link" id="facebook">
+                                        <img src="/src/images/facebook.svg" alt="Link to our Facebook">
+                                    </a>
+                                </li>
+                                <li class="footer__social__item">
+                                    <a href="" class="footer__social__link" id="instagram">
+                                        <img src="/src/images/instagram.svg" alt="Link to our Instagram">
+                                    </a>
+                                </li>
+                                <li class="footer__social__item">
+                                    <a href="" class="footer__social__link" id="twitter">
+                                        <img src="/src/images/twitter.svg" alt="Link to our Twitter">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="footer__right_column">
+                        <div class="footer_links__widget__container">
+                            <ul class="widget_column" id="footer_links">
+                                <li class="widget_title" id="footer_links">
+                                    <span class="widget_title" id="footer_links">Helpful Links</span>
+                                </li>
+                                <ul class="widget_list" id="footer_links">
+                                    <li class="widget_item" id="footer_links">
+                                        <a href="/front-end/src/about-us.html" class="widget_link" id="about_us" target="blank">About Us</a>
+                                    </li>
+                                    <li class="widget_item" id="footer_links">
+                                        <a href="" class="widget_link" id="repo" target="blank">GitHub Repo</a>
+                                    </li>
+                                </ul>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        `;
+    }
+}
+
+customElements.define('footer-component', Footer);
