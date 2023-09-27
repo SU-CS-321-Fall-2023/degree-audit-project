80213b210534fb62f9742264f86ecdc85b74b65d
sydheim <77406927+sydheim@users.noreply.github.com>
2023-09-26 19:45:20 -0700

    Reuploaded - attempt to connect database

--- back-end/Main.java
+++ back-end/Main.java
@@ -1,14 +1,20 @@
 import java.util.*;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import java.io.FileReader;
+import java.io.IOException;
 
 class Course {
     private String courseId;
     private String courseName;
     private int creditHours;
+    private int semester;
 
-    public Course(String courseId, String courseName, int creditHours) {
+    public Course(String courseId, String courseName, int creditHours, int semester) {
         this.courseId = courseId;
         this.courseName = courseName;
         this.creditHours = creditHours;
+        this.semester = semester;
     }
 
     public String getCourseName() {
@@ -21,12 +27,14 @@ class Course {
 class Major {
     private String majorId;
     private String majorName;
-    private List<Course> mandatoryCourses;
+    //private List<Course> mandatoryCourses;
+    private Map<Integer, List<Course>> coreCoursesBySemester;
 
     public Major(String majorId, String majorName) {
         this.majorId = majorId;
         this.majorName = majorName;
-        this.mandatoryCourses = new ArrayList<>();
+        //this.mandatoryCourses = new ArrayList<>();
+        this.coreCoursesBySemester = new HashMap<>();
     }
 
     // Getter for majorName
@@ -34,12 +42,20 @@ class Major {
         return majorName;
     }
 
-    public void addMandatoryCourse(Course course) {
-        mandatoryCourses.add(course);
-    }
+    //public void addMandatoryCourse(Course course) {
+        //mandatoryCourses.add(course);
+    //}
 
-    public List<Course> getMandatoryCourses() {
-        return mandatoryCourses;
+    public void addCoreCourses(int semester, List<Course> coreCourses){
+        coreCoursesBySemester.put(semester, coreCourses);
+    }
+    //public List<Course> getMandatoryCourses() {
+       // return mandatoryCourses;
+    //}
+
+    // Get core courses for a specific semester
+    public List<Course> getCoreCourses(int semester) {
+        return coreCoursesBySemester.get(semester);
     }
 
     // ... (other methods)
@@ -83,18 +99,43 @@ class Student {
 
 public class Main {
     public static void main(String[] args) {
-        Course math101 = new Course("MATH101", "Introduction to Mathematics", 3);
-        Course eng101 = new Course("ENG101", "English Composition", 3);
-        Course cs101 = new Course("CS101", "Introduction to Computer Science", 3);
+        String csvFilePath = "C:\\Users\\sydne\\OneDrive - Stetson University, Inc\\Desktop\\Academic Programs - Stetson University.csv";
+
+        try (CSVReader reader = new CSVReader(new FileReader(csvFilePath))){
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null){
+                //process the data as needed
+                String majorName = nextLine[0];
+                //other fields
+                System.out.println("Major: " + majorName);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Course math101 = new Course("MATH101", "Introduction to Mathematics", 4, 1);
+        Course eng101 = new Course("ENG101", "English Composition", 4, 1);
+        Course cs101 = new Course("CS101", "Introduction to Computer Science", 4, 1);
+
+        Course math201 = new Course("MATH201", "Advanced Mathematics", 4, 2);
+        Course eng201 = new Course("ENG201", "Advanced English Composition", 4, 2);
+        Course cs201 = new Course("CS201", "Advanced Computer Science", 4, 2);
 
         Major mathMajor = new Major("MathMajor", "Mathematics");
-        mathMajor.addMandatoryCourse(math101);
+        //mathMajor.addMandatoryCourse(math101);
+        mathMajor.addCoreCourses(1, Arrays.asList(math101));
+        mathMajor.addCoreCourses(2, Arrays.asList(math201));
 
         Major engMajor = new Major("EngMajor", "English");
-        engMajor.addMandatoryCourse(eng101);
+        //engMajor.addMandatoryCourse(eng101);
+        engMajor.addCoreCourses(1, Arrays.asList(eng101));
+        engMajor.addCoreCourses(2, Arrays.asList(eng201));
 
         Major csMajor = new Major("CSMajor", "Computer Science");
-        csMajor.addMandatoryCourse(cs101);
+        //csMajor.addMandatoryCourse(cs101);
+        csMajor.addCoreCourses(1, Arrays.asList(cs101));
+        csMajor.addCoreCourses(2, Arrays.asList(cs201));
 
         Student student1 = new Student("8001", "Sydney Heimann", mathMajor);
         Student student2 = new Student("8002", "Maddy Cobb", engMajor);
@@ -109,8 +150,8 @@ public class Main {
         String studentId = scanner.nextLine();
 
         Student student = findStudentById(studentId, student1, student2, student3);
-        if (student != null) {
-            displayDegreeAudit(student);
+        if (student != null) { int semester = 1; //replace with desired semester value
+            displayDegreeAudit(student,semester);
         } else {
             System.out.println("Student not found.");
         }
@@ -125,16 +166,17 @@ public class Main {
         return null; // Student not found
     }
 
-    public static void displayDegreeAudit(Student student) {
-        System.out.println("Degree Audit for " + student.getStudentName() + " (Major: " + student.getMajor().getMajorName() + "):");
+    public static void displayDegreeAudit(Student student, int semester) {
+        System.out.println("Degree Audit for " + student.getStudentName() + " (Major: " + student.getMajor().getMajorName() + ", Semester: " + semester + "):");
         Set<Course> completedCourses = student.getCompletedCourses();
-        List<Course> mandatoryCourses = student.getMajor().getMandatoryCourses();
+        //List<Course> mandatoryCourses = student.getMajor().getMandatoryCourses();
+        List<Course> coreCourses = student.getMajor().getCoreCourses(semester);
 
-        for (Course mandatoryCourse : mandatoryCourses) {
-            if (!completedCourses.contains(mandatoryCourse)) {
-                System.out.println(" - " + mandatoryCourse.getCourseName() + " (Incomplete)");
+        for (Course coreCourse : coreCourses) {
+            if (!completedCourses.contains(coreCourse)) {
+                System.out.println(" - " + coreCourse.getCourseName() + " (Incomplete)");
             } else {
-                System.out.println(" - " + mandatoryCourse.getCourseName() + " (Complete)");
+                System.out.println(" - " + coreCourse.getCourseName() + " (Complete)");
             }
         }
     }
