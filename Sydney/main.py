e40550d664bc241329f5a1a117b88f2e721da7c6
sydheim <77406927+sydheim@users.noreply.github.com>
2023-10-04 22:36:33 -0400

    Update main.py

--- Sydney/main.py
+++ Sydney/main.py
@@ -1,3 +1,45 @@
+#import os
+#import csv
+#from flask import Flask, render_template, request
+
+#app = Flask(__name__)
+
+#UPLOAD_DIRECTORY = 'C:/Users/sydne/OneDrive - Stetson University, Inc/Desktop/Computer Science/degree-audit-project/back-end'
+#app.config['UPLOAD_FOLDER'] = UPLOAD_DIRECTORY
+
+#@app.route('/', methods=['GET', 'POST'])
+#def index():
+ #   data = []  # A list to hold our CSV data
+
+  #  if request.method == 'POST':
+   #     student_id = request.form.get('student_id')
+    #    print(f"Received student ID: {student_id}")
+
+        # Get the CSV path based on student ID
+     #   csv_path = get_csv_path_for_student(student_id)
+      #  print(f"Looking for CSV at path: {csv_path}")
+
+       # if not os.path.exists(csv_path):
+        #    print(f"CSV not found for student ID: {student_id}")
+            # Handle the case where the CSV file doesn't exist for the given ID
+         #   return "No data available for this student ID.", 400
+
+        # Read the CSV file and store the data
+        #with open(csv_path, 'r') as f:
+         #   reader = csv.reader(f)
+          #  data = [row for row in reader]
+
+   # return render_template('index.html', data=data)
+
+#def get_csv_path_for_student(student_id):
+    # This will attempt to find a CSV with the name "studentID.csv" in your UPLOAD_DIRECTORY
+    # Adjust this logic based on how your files are organized/named
+ #   return os.path.join(app.config['UPLOAD_FOLDER'], f"{student_id}.csv")
+
+#if __name__ == '__main__':
+ #   app.run(host='192.168.1.216', port=5000, debug=True)
+
+
 import os
 import csv
 from werkzeug.utils import secure_filename
@@ -42,8 +84,10 @@ def index():
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='192.168.1.216', port=5000, debug=True)
     #app.run(debug=True)
+
+
 
 
 
