0ad5bcc1eebac4d2179ff1e03b28c9a775d81ff3
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-10-09 11:36:37 -0400

    Combined original weblet.py with Syndey's version
    11:36 AM, 10/09/2023 - Alexander Garofalo
    
    Combined the original flask-devop Flask web app with Sydney's version. Placed all the necessary files into the front-end/src/ folder.

--- front-end/src/weblet copy.py
+++ front-end/src/weblet copy.py
@@ -9,6 +9,8 @@ def ourPaths():
     in order for the site to build and run.
     
     ourPaths() must be called immediately after it ends.
+    
+    Purpose: Initializes necessary variable for web app.
     """
     global webletIndex
     webletIndex = 'index.html'
@@ -16,6 +18,10 @@ def ourPaths():
     global rootPath
     rootPath = os.path.abspath(os.getcwd()) + "/front-end/src"
 ourPaths() # Must be placed at beginning of file.
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 
 app = Flask(
@@ -25,18 +31,44 @@ app = Flask(
     template_folder="./templates",
     static_folder="./static")
 
+ALLOWED_EXTENSIONS = {'csv'}
 
 
-@app.route("/index")
+
+# @app.route("/index")
+# def index():
+#     return render_template(webletIndex)
+
+# @app.route('/data')
+# def database():
+#     return 'data'
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template(webletIndex)
+    data = []  # A list to hold our CSV data
 
-@app.route('/data')
-def database():
-    return 'data'
+    # Use raw string notation for the path or double up the backslashes
+    csv_path = rootPath + r'/Academic Programs - Stetson University.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Databasefinal.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Database.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\Academic_Programs_-_Stetson_University.csv'
 
+    if request.method == 'POST':
+        major = request.form.get('major', None)  # Fetch the major entered by the user
 
+        # Read the predefined CSV file and store the data
+        with open(csv_path, 'r') as f:
+            reader = csv.reader(f)
+            data = [row for row in reader]
 
+        # Filter the data based on the major if one is entered
+        if major:
+            # Assuming the major is in the 2nd column (index 1). Adjust the index if needed.
+            data = [data[0]] + [row for row in data[1:] if major.lower() in row[1].lower()]
+
+    return render_template(webletIndex, data=data)
 
 
 
