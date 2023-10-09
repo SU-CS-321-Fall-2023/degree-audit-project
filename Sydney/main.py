1eb89dc3b09dae6826333105e9a8d78fb6f1d276
sydheim <77406927+sydheim@users.noreply.github.com>
2023-10-09 11:11:45 -0400

    file move

--- Sydney/main.py
+++ Sydney/main.py
@@ -29,7 +29,7 @@
          #   reader = csv.reader(f)
           #  data = [row for row in reader]
 
-   # return render_template('index.html', data=data)
+    #return render_template('index.html', data=data)
 
 #def get_csv_path_for_student(student_id):
     # This will attempt to find a CSV with the name "studentID.csv" in your UPLOAD_DIRECTORY
@@ -51,43 +51,82 @@ app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = {'csv'}
 
-UPLOAD_DIRECTORY = 'C:/Users/sydne/OneDrive - Stetson University, Inc/Desktop/Computer Science/degree-audit-project/back-end'
+UPLOAD_DIRECTORY = 'C:/Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python'
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIRECTORY
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+
+# In-memory database of students
+#students_db = {
+ #   "800740212": {"name": "Sydney Heimann", "major": "Cybsersecurity", "courses_taken": "Math, Physics", "courses_needed": "Biology, Chemistry"},
+  #  "123": {"name": "Maddy Cobb", "major": "Computer Science", "courses_taken": "Accounting, Finance", "courses_needed": "Management, Marketing"}
+    # Add more student data as needed
+#}
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = []  # A list to hold our CSV data
 
-    if request.method == 'POST' and 'csv' in request.files:
-        file = request.files['csv']
-        if file.filename == '':
+    # Use raw string notation for the path or double up the backslashes
+    csv_path = r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\Academic_Programs_-_Stetson_University.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Databasefinal.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Database.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\Academic_Programs_-_Stetson_University.csv'
+
+    if request.method == 'POST':
+        major = request.form.get('major', None)  # Fetch the major entered by the user
+
+        # Read the predefined CSV file and store the data
+        with open(csv_path, 'r') as f:
+            reader = csv.reader(f)
+            data = [row for row in reader]
+
+        # Filter the data based on the major if one is entered
+        if major:
+            # Assuming the major is in the 2nd column (index 1). Adjust the index if needed.
+            data = [data[0]] + [row for row in data[1:] if major.lower() in row[1].lower()]
+
+    return render_template('index.html', data=data)
+
+
+##@app.route('/', methods=['GET', 'POST'])
+##def index():
+  ##  data = []  # A list to hold our CSV data
+
+    ##if request.method == 'POST' and 'csv' in request.files:
+      ##  file = request.files['csv']
+        ##if file.filename == '':
             # No selected file
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+          ##  return redirect(request.url)
+        ##if file and allowed_file(file.filename):
+          #3  filename = secure_filename(file.filename)
             # Construct the path to save the file
-            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            print("Saving to:", save_path)
-            file.save(save_path)
+            ##save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            ##print("Saving to:", save_path)
+            ##file.save(save_path)
 
             # Read the saved CSV file and store the data
-            with open(save_path, 'r') as f:
-                reader = csv.reader(f)
-                data = [row for row in reader]
+            ##with open(save_path, 'r') as f:
+              ##  reader = csv.reader(f)
+                ##data = [row for row in reader]
 
             # After reading the CSV data, redirect isn't necessary.
             # The next line renders the index.html with CSV data
 
-    return render_template('index.html', data=data)
+ #   student_details = None
+
+  #  if request.method == 'POST':
+   #     student_id = request.form.get('student_id', '')  # Get the student ID from form
+    #    student_details = student_db.get(student_id)  # Fetch the student details from the in-memory databse
+
+   ## return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.216', port=5000, debug=True)
+    app.run(host='10.66.3.179', port=5000, debug=True)
     #app.run(debug=True)
-
-
 
 
 
@@ -127,7 +166,7 @@ if __name__ == '__main__':
         #    print("Saving to:", save_path)  # Optional: to verify the save path
          #   file.save(save_path)
           #  return redirect(url_for('index'))
-    #return render_template('index.html')
+   # return render_template('index.html')
 
 #if __name__ == '__main__':
  #   app.run(debug=True)
@@ -167,8 +206,8 @@ if __name__ == '__main__':
       #      filename = secure_filename(file.filename)
        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         #    return redirect(url_for('index'))
-    #return render_template('index.html')
+   # return render_template('index.html')
 
 #if __name__ == '__main__':
-#    app.run(debug=True)
+ #   app.run(debug=True)
 
