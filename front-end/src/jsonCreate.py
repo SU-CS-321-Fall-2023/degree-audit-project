008c3d350b83ab63d2c9637dd0c72777e2867327
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-10-12 12:24:09 -0400

    Split user.csv into two
    12:24 PM, 10/12/2023 - Alexander Garofalo
    
    Like I mentioned in my previous commit, I will be pushing the other content I have been working on.
    
    Nonso had taken the two HTML tables with placeholder text in index.html and converted them into CSV format, having saved it to user.csv.
    
    In this commit, I am simply splitting that CSV file into two, one for each table: "user.csv" and "reqs.csv" .
    
    I also updated trimmer.py and jsonCreate.py with some comments.

--- front-end/src/jsonCreate.py
+++ front-end/src/jsonCreate.py
@@ -6,6 +6,8 @@ session = requests.Session()
 # Both of these cookies are necessary to access the URL.
 session.cookies['JSESSIONID'] = 'your JSESSIONID'
 session.cookies['INGRESSCOOKIE'] = 'your INGRESSCOOKIE'
+# You will need to get your own personal cookies.
+#! We don't condone cookie sharing here!!
 
 # url = 'https://myapps.stetson.edu/StudentRegistrationSsb/ssb/registration'
 data_url = 'https://myapps.stetson.edu/StudentRegistrationSsb/ssb/registrationHistory/reset?term=202315'
@@ -17,7 +19,7 @@ try:
     if response.status_code == 200:
         print("Request was successful.")
         data = response.json()
-        with open('student_data.json', 'w') as json_file:
+        with open('Output Files/student_data.json', 'w') as json_file:
             json.dump(data, json_file, indent=4)
         print("student_data.json has been created.")
         # print(response.text)  # Print the content of the page
