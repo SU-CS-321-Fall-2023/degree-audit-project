3e9a4d53ad7ea404035f3ae282fe72ca5475e53e
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-10-12 13:26:16 -0400

    Updated weblet.py versions
    1:26 PM, 10/12/2023 - Alexander Garofalo
    
    I created weblet.py as an original draft for a Flask web app that would render the HTML and CSS files. When Sydney came out with her working Flask web app that was rendering the CSV files, I began working on combining my version with her version.
    
    To keep a history of the original, I made a copy of my version, so I had two: "weblet.py" and "weblet copy.py". With "weblet.py" as the original, I made "weblet copy.py" the combination of the two versions.
    
    Later, once I was done combining them, I renamed "weblet.py" to "weblet_OLD.py", commented out all its code, and moved it to the "Deprecated" folder I created. I then renamed "weblet copy.py" to "weblet.py".

--- front-end/src/Smart Degree Audit/weblet.py
+++ front-end/src/Smart Degree Audit/weblet.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template
-# from flask import url_for
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, url_for
+import csv
 import os
 
 def ourPaths():
@@ -9,13 +9,20 @@ def ourPaths():
     in order for the site to build and run.
     
     ourPaths() must be called immediately after it ends.
+    
+    Purpose: Initializes necessary variable for web app.
     """
     global webletIndex
     webletIndex = 'index.html'
     
     global rootPath
-    rootPath = os.path.abspath(os.getcwd()) + "/front-end/src"
+    rootPath = os.path.abspath(os.getcwd()) + "\\front-end\\src"
+    print(f"rootPath: {rootPath}")
 ourPaths() # Must be placed at beginning of file.
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 
 app = Flask(
@@ -25,18 +32,65 @@ app = Flask(
     template_folder="./templates",
     static_folder="./static")
 
+ALLOWED_EXTENSIONS = {'csv'}
 
 
-@app.route("/index")
+
+# @app.route("/index")
+# def index():
+#     return render_template(webletIndex)
+
+# @app.route('/data')
+# def database():
+#     return 'data'
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template(webletIndex)
+    data = []  # A list to hold our CSV data
 
-@app.route('/data')
-def database():
-    return 'data'
+    # Use raw string notation for the path or double up the backslashes
+    userCSV_path = rootPath + r'\\user.csv'
+    reqsCSV_path = rootPath + r'\\reqs.csv'
+    testCSV_path = rootPath + r'\\test.csv'
+
+    if request.method == 'POST':
+        email = request.form.get('student_email', None)  # Fetch the major entered by the user
+
+        # Read the predefined CSV file and store the data
+        with open(testCSV_path, 'r') as f:
+            reader = csv.reader(f)
+            data = [row for row in reader]
+
+        # Filter the data based on the major if one is entered
+        if email:
+            # Assuming the major is in the 2nd column (index 1). Adjust the index if needed.
+            data = [data[0]] + [row for row in data[1:] if email.lower() in row[1].lower()]
+
+    return render_template(webletIndex, data=data)
 
 
+# Sydney's log
+    # csv_path = rootPath + r'\\Academic Programs - Stetson University.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Databasefinal.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Database.csv'
+    #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\Academic_Programs_-_Stetson_University.csv'
 
+    # if request.method == 'POST':
+    #     major = request.form.get('major', None)  # Fetch the major entered by the user
+
+    #     # Read the predefined CSV file and store the data
+    #     with open(csv_path, 'r') as f:
+    #         reader = csv.reader(f)
+    #         data = [row for row in reader]
+
+    #     # Filter the data based on the major if one is entered
+    #     if major:
+    #         # Assuming the major is in the 2nd column (index 1). Adjust the index if needed.
+    #         data = [data[0]] + [row for row in data[1:] if major.lower() in row[1].lower()]
+
+    # return render_template(webletIndex, data=data)
 
 
 
@@ -44,4 +98,4 @@ if __name__ == '__main__':
     """
     Must be placed at the end of the file.
     """
-    app.run(host = '127.0.0.1', port = 8000, debug = True)
+    app.run(host = '10.66.3.41', port = 8000, debug = True)
