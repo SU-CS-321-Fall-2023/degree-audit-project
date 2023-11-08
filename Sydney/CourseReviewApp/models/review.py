ec2b4e8be22f5f120e86f7617459ddea91cb9779
sydheim <77406927+sydheim@users.noreply.github.com>
2023-11-08 11:16:17 -0500

    deletions and clean up

--- Sydney/CourseReviewApp/models/review.py
+++
@@ -1,4 +0,0 @@
-class Review:
-    def __init__(self, rating, comment):
-        self.rating = rating
-        self.comment = comment
--- Sydney/CourseReviewApp/storage/persistence.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def save_data(data, filename="data.pkl"):
-    with open(filename, 'wb') as file:
-        pickle.dump(data, file)
-
-def load_data(filename="data.pkl"):
-    with open(filename, 'rb') as file:
-        return pickle.load(file)
