d24f2184e2e789d9c9199c07ccc3e0a87a9b3a39
sydheim <77406927+sydheim@users.noreply.github.com>
2023-11-08 10:10:58 -0500

    dark mode/light mode
    Featured on the Wikipedia page example

---
+++ Sydney/Wikipedia/app.py
@@ -0,0 +1,23 @@
+#Test to upload
+# import necessary libraries 
+from flask import Flask, request, render_template 
+import wikipedia
+  
+app = Flask(__name__) 
+  
+# create HOME View 
+@app.route("/", methods=["POST", "GET"]) 
+def home(): 
+    if request.method == "GET": 
+        return render_template("index.html") 
+    else: 
+        search = request.form["search"] 
+  
+        # Fetch data from wikipedia 
+        result = wikipedia.summary(search, sentences=2) 
+        title = search
+        return render_template("results.html", title=title, summary=result)
+        #return f"<h1>{result}</h1>"
+  
+if __name__ == '__main__': 
+    app.run(debug=True) 
