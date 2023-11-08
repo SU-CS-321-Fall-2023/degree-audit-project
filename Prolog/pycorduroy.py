1dd7f5656387253c080e3647bf79a60432dccf2d
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-11-08 05:51:11 -0500

    Update pycorduroy.py
    05:55 AM, 11/08/2023 - Alexander Garofalo
    
    Completed two of the TODO statements and added a fourth. Updated how the program would be reading the outputted files from corduroy.pro (Prolog). I have compiled the outputted files into a list, which is then run through a for-loop that puts all the degrees into one comprehensive list. This list is then stored in a file: majorsSorted.txt
    
    Also, I am uploading all the output files created by corduroy.pro (Prolog) and pycorduroy.py (Python).

--- Prolog/pycorduroy.py
+++ Prolog/pycorduroy.py
@@ -1,37 +1,79 @@
 import os
 
 # TODO: Call SWI-Prolog from this file.
-# TODO: After opening program, consult "corduroy.pro"
-# TODO: Run query to create "majors.txt"
+# // TODO: After opening program, consult "corduroy.pro"
+# // TODO: Run queries to create lists of majors
+# TODO: Break code into functions.
 
 # Replace the file paths with your own
-majors = "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/majors.txt"
+degrees = [
+    "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/baMajors.txt",
+    "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/bsMajors.txt",
+    "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/bbaMajors.txt",
+    "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/bmMajors.txt",
+    "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/bmeMajors.txt"
+]
+baMajors  = degrees[0]
+bsMajors  = degrees[1]
+bbaMajors = degrees[2]
+bmMajors  = degrees[3]
+bmeMajors = degrees[4]
+
+allMajors    = "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/allMajors.txt"
 majorsSorted = "D:/GitHub/Software Dev II/The-Auditors/degree-audit-project/Prolog/majorsSorted.txt"
 
 contents = ''
+for i in range(len(degrees)):
+    if os.path.isfile(degrees[i]):
+        print(f"'{degrees[i]}' is being read....")
+        with open(degrees[i], "r") as myFile:
+            while True:
+                char = myFile.read(1)
+                if not char:
+                    break
+                match char:
+                    case '[':
+                        contents += ""
+                    case '(':
+                        contents += "["
+                    case ')':
+                        contents += "] "
+                    case ']':
+                        contents += ""
+                    case _:
+                        contents += char
+        contents += "\n"
+        myFile.close() # Ensures that no streams are left open.
+print(f"contents:\n{contents}")
+contentsListed = contents.split(" ,")
+print(f"contentsListed:\n{contentsListed}")
 
-if os.path.isfile(majors):
-    print(f"'{majors}' is being read....")
-    with open(majors, "r") as myFile:
-        while 1:
+fullContents = ''
+if os.path.isfile(allMajors):
+    print(f"'{allMajors}' is being read....")
+    with open(allMajors, "r") as myFile:
+        while True:
             char = myFile.read(1)
             if not char:
                 break
             match char:
                 case '[':
-                    contents += ""
+                    fullContents += ""
                 case '(':
-                    contents += "["
+                    fullContents += "["
                 case ')':
-                    contents += "] "
+                    fullContents += "] "
                 case ']':
-                    contents += ""
+                    fullContents += ""
                 case _:
-                    contents += char
-        myFile.close()
-print(contents)
-contentsListed = contents.split(" ,",)
-print(contentsListed)
+                    fullContents += char
+    fullContents += "\n"
+    myFile.close() # Ensures that no streams are left open.
+print()
+print(f"fullContents:\n{fullContents}")
+fullContentsListed = fullContents.split(" ,")
+print(f"fullContentsListed:\n{fullContentsListed}")
+
 print()
 
 # This little snippet is from my Senior Project -- Alexander Garofalo
@@ -43,8 +85,8 @@ else:
     print(f"Error!! '{majorsSorted}' was not found.")
 # END OF SNIPPET
 
-for i in range(len(contentsListed)):
+for i in range(len(fullContentsListed)):
     with open(majorsSorted, "a") as myFile:
-        myFile.write(f"{contentsListed[i]}\n")
+        myFile.write(f"{fullContentsListed[i]}\n")
 
-myFile.close()
+myFile.close() # Ensures that no streams are left open.
