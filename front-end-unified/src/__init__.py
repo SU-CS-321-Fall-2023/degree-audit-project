027c0915a5461d7031ee0f3f72d5140f6c3d7fc0
Madison Cobb <89548631+madz777@users.noreply.github.com>
2023-11-19 21:20:27 -0500

    consolidating frontend
    Flask Server Initialized
    Webapp Design Planned

---
+++ front-end-unified/src/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+def create_app():
+    rootPath = os.path.abspath(os.getcwd())
+    app = Flask(
+        __name__,
+        root_path = rootPath,
+        template_folder= rootPath + "/front-end/src/templates",
+        static_folder= rootPath + "/front-end/src/static")
+    app.config['SECRET_KEY'] = 'hai'
+
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+    
+    return app
