df1d5f1923848717146c1fa1756a08d94b3e23fd
madz777 <maddyc322@gmail.com>
2023-11-28 15:46:04 -0500

    ++views

--- front-end-unified/views.py
+++ front-end-unified/views.py
@@ -4,4 +4,12 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Nyah</h1>"
+    return "<h1>Homepage</h1>"
+
+@views.route('/reviews')
+def reviews():
+    return "<h1>Course Reviews Page</h1>"
+
+@views.route('/cultural')
+def culture():
+    return "<h1>Cultural Credits Opportunity Tracker</h1>"
