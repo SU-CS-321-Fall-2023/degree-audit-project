c8ab00a92164690256bb357bfb24debd4a634699
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-12-04 09:35:14 -0500

    Update app.py (Course Review)
    09:35 AM, 12/04/2023 - Alexander Garofalo
    
    I added the MySQL connectors to the file to allow it to connect to the database, just like how I did with the Academic Progress Tracker.
    
    I also adjusted the spacing for the form fields to make all the equal signs line up with the rest of its respective cluster. I figured that this would help increase the code's readability. In case you all don't like it, I copied the code and placed the original arrangements as comments below.

--- back-end/Sydney/CourseReviewApp/app.py
+++ back-end/Sydney/CourseReviewApp/app.py
@@ -5,52 +5,110 @@ from wtforms import StringField, TextAreaField, SubmitField, RadioField
 from wtforms.validators import DataRequired, Length
 from datetime import datetime
 from flask import send_from_directory
+
+import mysql.connector
 import os
+rootPath = os.path.abspath(os.getcwd())
 
-# import os
-# rootPath = os.path.abspath(os.getcwd())
+pwFile = (rootPath + "\\ourPySQL.txt")
+with open(pwFile, 'r') as passFile:
+    password = passFile.readline()
+    # Password for the databases gets read from a file, so
+    # that it is not explicitly stored here in the code.
+    # Also, the databases are currently located locally on 
+    # my machine (Alexander G.) and I do not feel inclined 
+    # to allowing the entire world have free root access to 
+    # my computer's databases. MySQL Injections are scary.
 
-app = Flask(__name__)
+catalog = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=password,
+    database="catalog"
+)
+reviews = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=password,
+    database="reviews"
+)
+myCatalog = catalog.cursor(prepared=True)
+myReviews = reviews.cursor(prepared=True)
+
+
+app = Flask(
+    __name__,
+    root_path=(rootPath),
+    template_folder= rootPath + "/back-end/Sydney/CourseReviewApp/templates",
+    static_folder= rootPath + "/back-end/Sydney/CourseReviewApp/static")
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reviews.db'
 app.config['SECRET_KEY'] = 'your_secret_key'
 
 db = SQLAlchemy(app)
 
+
+
 class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id          = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String(200), unique=True, nullable=False)
-    reviews = db.relationship('Review', backref='course', lazy=True)
+    reviews     = db.relationship('Review', backref='course', lazy=True)
+    # id = db.Column(db.Integer, primary_key=True)
+    # course_name = db.Column(db.String(200), unique=True, nullable=False)
+    # reviews = db.relationship('Review', backref='course', lazy=True)
 
 class Review(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
-    review_text = db.Column(db.Text, nullable=False)
+    id                 = db.Column(db.Integer, primary_key=True)
+    course_id          = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    review_text        = db.Column(db.Text, nullable=False)
     professor_feedback = db.Column(db.Text, nullable=True)
-    course_load = db.Column(db.Text, nullable=True)
-    quizzes_tests = db.Column(db.Text, nullable=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    course_load        = db.Column(db.Text, nullable=True)
+    quizzes_tests      = db.Column(db.Text, nullable=True)
+    timestamp          = db.Column(db.DateTime, default=datetime.utcnow)
+    # id = db.Column(db.Integer, primary_key=True)
+    # course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    # review_text = db.Column(db.Text, nullable=False)
+    # professor_feedback = db.Column(db.Text, nullable=True)
+    # course_load = db.Column(db.Text, nullable=True)
+    # quizzes_tests = db.Column(db.Text, nullable=True)
+    # timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
     # New fields
-    instructor_notes_online = db.Column(db.String(3))
+    instructor_notes_online       = db.Column(db.String(3))
     instructor_offer_extra_credit = db.Column(db.String(3))
-    instructor_flexibility = db.Column(db.String(15))
-    instructor_demeanor = db.Column(db.String(250))
+    instructor_flexibility        = db.Column(db.String(15))
+    instructor_demeanor           = db.Column(db.String(250))
+    # instructor_notes_online = db.Column(db.String(3))
+    # instructor_offer_extra_credit = db.Column(db.String(3))
+    # instructor_flexibility = db.Column(db.String(15))
+    # instructor_demeanor = db.Column(db.String(250))
     # Add other new fields as needed...
 
 class ReviewForm(FlaskForm):
-    course_name = StringField('Course Name:', validators=[DataRequired()])
-    review_text = TextAreaField('Overall Course Review:', validators=[DataRequired()])
-    professor_rate = RadioField('Please rate the Professor:', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
+    course_name        = StringField('Course Name:', validators=[DataRequired()])
+    review_text        = TextAreaField('Overall Course Review:', validators=[DataRequired()])
+    professor_rate     = RadioField('Please rate the Professor:', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
     professor_feedback = TextAreaField('What did you think of the Professor:', validators=[DataRequired()])
-    course_load = TextAreaField('Describe the course load:', validators=[DataRequired()])
-    quizzes_tests = TextAreaField('Describe quizzes/tests (if any):', validators=[DataRequired()])
-    submit = SubmitField('Submit Review')
+    course_load        = TextAreaField('Describe the course load:', validators=[DataRequired()])
+    quizzes_tests      = TextAreaField('Describe quizzes/tests (if any):', validators=[DataRequired()])
+    submit             = SubmitField('Submit Review')
+    # course_name = StringField('Course Name:', validators=[DataRequired()])
+    # review_text = TextAreaField('Overall Course Review:', validators=[DataRequired()])
+    # professor_rate = RadioField('Please rate the Professor:', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
+    # professor_feedback = TextAreaField('What did you think of the Professor:', validators=[DataRequired()])
+    # course_load = TextAreaField('Describe the course load:', validators=[DataRequired()])
+    # quizzes_tests = TextAreaField('Describe quizzes/tests (if any):', validators=[DataRequired()])
+    # submit = SubmitField('Submit Review')
 
     # New form fields
-    instructor_notes_online = RadioField('Does the instructor post the class notes online?', choices=[('Yes', 'Yes'), ('No', 'No'), ('N/A', 'N/A')])
+    instructor_notes_online       = RadioField('Does the instructor post the class notes online?', choices=[('Yes', 'Yes'), ('No', 'No'), ('N/A', 'N/A')])
     instructor_offer_extra_credit = RadioField('Does the instructor offer extra credit?', choices=[('Yes', 'Yes'), ('No', 'No')])
-    instructor_flexibility = RadioField('The instructor is flexible when it comes to unforeseen circumstances:', choices=[('Strongly Disagree', 'Strongly Disagree'), ('Disagree', 'Disagree'), ('Neutral', 'Neutral'), ('Agree', 'Agree'), ('Strongly Agree', 'Strongly Agree'), ('N/A', 'N/A')])
-    instructor_demeanor = TextAreaField('What was the professor\'s demeanor like?', validators=[Length(max=250)])
+    instructor_flexibility        = RadioField('The instructor is flexible when it comes to unforeseen circumstances:', choices=[('Strongly Disagree', 'Strongly Disagree'), ('Disagree', 'Disagree'), ('Neutral', 'Neutral'), ('Agree', 'Agree'), ('Strongly Agree', 'Strongly Agree'), ('N/A', 'N/A')])
+    instructor_demeanor           = TextAreaField('What was the professor\'s demeanor like?', validators=[Length(max=250)])
+    # instructor_notes_online = RadioField('Does the instructor post the class notes online?', choices=[('Yes', 'Yes'), ('No', 'No'), ('N/A', 'N/A')])
+    # instructor_offer_extra_credit = RadioField('Does the instructor offer extra credit?', choices=[('Yes', 'Yes'), ('No', 'No')])
+    # instructor_flexibility = RadioField('The instructor is flexible when it comes to unforeseen circumstances:', choices=[('Strongly Disagree', 'Strongly Disagree'), ('Disagree', 'Disagree'), ('Neutral', 'Neutral'), ('Agree', 'Agree'), ('Strongly Agree', 'Strongly Agree'), ('N/A', 'N/A')])
+    # instructor_demeanor = TextAreaField('What was the professor\'s demeanor like?', validators=[Length(max=250)])
     # Add other new form fields as needed...
 
 @app.route('/', methods=['GET', 'POST'])
@@ -165,4 +223,5 @@ def show_reviews(course_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    # app.run(host='192.168.1.46', port=3000, debug=True)
+    app.run(host='127.0.0.1', port=3000, debug=True)
