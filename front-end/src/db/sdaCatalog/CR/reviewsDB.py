8d66cec4f4054c0b6f4e33ae6e4f678be01cbdbb
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-12-04 09:17:25 -0500

    Create review.py and reviewsDB.py
    09:17 AM, 12/04/2023 - Alexander Garofalo
    
    I am working on creating a script that will auto-create the MySQL table to store the reviews in. Basically, it will automatically format it with the columns, so it is less manual work on my end.
    
    This is basically what I was doing with the sdaCatalog and the other MySQL databases and tables.

---
+++ front-end/src/db/sdaCatalog/CR/reviewsDB.py
@@ -0,0 +1,61 @@
+import json
+
+null = None
+reviews = list()
+try:
+    with open('front-end\\src\\db\\sdaCatalog\\sdaCatalog.json', 'r') as ourFile:
+        ourData = json.load(ourFile)
+        for x in range(len(ourData)):
+            app_data = {
+                "entry": ourData[x]["entry"],
+                "id": ourData[x]["id"],
+                # "termEffective": ourData[x]["termEffective"],
+                "courseNumber": ourData[x]["courseNumber"],
+                "subject": ourData[x]["subject"],
+                "subjectCode": ourData[x]["subjectCode"],
+                "college": ourData[x]["college"],
+                "collegeCode": ourData[x]["collegeCode"],
+                "department": ourData[x]["department"],
+                "departmentCode": ourData[x]["departmentCode"],
+                "courseTitle": ourData[x]["courseTitle"],
+                # "durationUnit": ourData["data"][x]["durationUnit"],
+                # "numberOfUnits": ourData["data"][x]["numberOfUnits"],
+                "attributes": ourData[x]["attributes"],
+                # "gradeModes": ourData["data"][x]["gradeModes"],
+                # "ceu": ourData[x]["ceu"],
+                # "courseScheduleTypes": ourData[x]["courseScheduleTypes"],
+                # "courseLevels": ourData["data"][x]["courseLevels"],
+                # "creditHourLow": ourData[x]["creditHourLow"],
+                # "creditHourHigh": ourData[x]["creditHourHigh"],
+                # "creditHourIndicator": ourData[x]["creditHourIndicator"],
+                # "lectureHourLow": ourData[x]["lectureHourLow"],
+                # "lectureHourHigh": ourData[x]["lectureHourHigh"],
+                # "lectureHourIndicator": ourData[x]["lectureHourIndicator"],
+                # "billHourLow": ourData[x]["billHourLow"],
+                # "billHourHigh": ourData[x]["billHourHigh"],
+                # "billHourIndicator": ourData[x]["billHourIndicator"],
+                # "labHourLow": ourData[x]["labHourLow"],
+                # "labHourHigh": ourData[x]["labHourHigh"],
+                # "labHourIndicator": ourData[x]["labHourIndicator"],
+                # "otherHourLow": ourData[x]["otherHourLow"],
+                # "otherHourHigh": ourData[x]["otherHourHigh"],
+                # "otherHourIndicator": ourData[x]["otherHourIndicator"],
+                # "description": ourData[x]["description"],
+                # "subjectDescription": ourData[x]["subjectDescription"],
+                # "courseDescription": ourData[x]["courseDescription"],
+                # "division": ourData["data"][x]["division"],
+                # "termStart": ourData[x]["termStart"],
+                # "termEnd": ourData[x]["termEnd"],
+                # "preRequisiteCheckMethodCde": ourData[x]["preRequisiteCheckMethodCde"],
+                # "anySections": ourData["data"][x]["anySections"],
+                # "prerequisites": ourData[x]["prerequisites"]
+            }
+            reviews.append(app_data)
+        # print("\n".join(map(str, reviews)))
+        with open('front-end\\src\\db\\sdaCatalog\\sdaReviews.json', 'w') as json_file:
+            json.dump(reviews, json_file, indent=4)
+        print("sdaReviews.json has been created.")
+        # print(myIDs)
+
+except json.JSONDecodeError as e:
+    print(f"JSON decoding error: {e}")
