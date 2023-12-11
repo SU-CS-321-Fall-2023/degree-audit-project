3971b684389dc6e2bd451783dd1af97b8f26101a
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-12-10 22:03:05 -0500

    Update courseReviews.py
    10:03 PM, 12/10/2023 - Alexander Garofalo
    
    Kept the wrong version in a Git Merge. My bad.

--- front-end/src/blueprints/Course_Reviews/courseReviews.py
+++ front-end/src/blueprints/Course_Reviews/courseReviews.py
@@ -1,10 +1,7 @@
-# from werkzeug.utils import secure_filename
-# from flask import Flask, jsonify, redirect, url_for, send_from_directory
-# import csv
-from flask import render_template, request, redirect, url_for, flash
-from flask import send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, Blueprint, render_template, redirect, url_for
+from flask import flash, request
 
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 
 from wtforms import StringField, TextAreaField, SubmitField, RadioField
@@ -12,12 +9,9 @@ from wtforms.validators import DataRequired, Length
 
 from datetime import datetime
 
-from flask_cors import CORS
-import os
 import mysql.connector
 
-from src import create_app
-#Sydney
+# from weblet import rootPath, passwordTA, passwordTC, weblet
 def ourPaths():
     """
     ourPaths() must be placed at the beginning of the file 
@@ -27,53 +21,42 @@ def ourPaths():
     
     Purpose: Initializes necessary variable for web app.
     """
+    import os
+
     global rootPath
-    # rootPath = os.path.abspath(os.getcwd()) + "\\front-end\\src"
     rootPath = os.path.abspath(os.getcwd())
-    print(f"rootPath: {rootPath}")
-    
-    # global webletIndex
-    # webletIndex =  "index.html"
-    
-    global password
-    # pwFile = (rootPath + "\\ourPySQL.txt")
-    pwFile = (rootPath + "\\Misc_Folder\\SQL\\ourPySQL.txt")
+
+    global passwordTA
+    global passwordTC
+    pwFile = (rootPath + "\\Misc_Folder\\SQL\\TA_ourPySQL.txt")
     with open(pwFile, 'r') as passFile:
-        password = passFile.readline()
+        passwordTA = passFile.readline()
+    pwFile = (rootPath + "\\Misc_Folder\\SQL\\TC_ourPySQL.txt")
+    with open(pwFile, 'r') as passFile:
+        passwordTC = passFile.readline()
         # Password for the databases gets read from a file, so
         # that it is not explicitly stored here in the code.
-        # Also, the databases are currently located locally on 
-        # my machine (Alexander G.) and I do not feel inclined 
-        # to allowing the entire world have free root access to 
-        # my computer's databases. MySQL Injections are scary.
-ourPaths()# Must be placed at beginning of file.
+        # MySQL Injections are scary.
 
-# Connections to the MySQL Databases
-# catalog = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password=password,
-#     database="catalog"
-# )
-# reviews = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password=password,
-#     database="reviews"
-# )
-# myCatalog = catalog.cursor(prepared=True)
-# myReviews = reviews.cursor(prepared=True)
+    print("Loading '/Course-Reviews'...")
+ourPaths() # Must be placed at beginning of file.
 
-app = create_app()
-# cors = CORS(app)
-# Configure the value of the "origins" key to be the actual URL of the React Frontend.
-# Make sure to NOT have a / at the end of the "origins" URL.
-cors = CORS(app, resources={r"/*": {"origins": "http://localhost:*"}})
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reviews.db'
-app.config['SECRET_KEY'] = 'your_secret_key'
+# weblet.register_blueprint(courseReviews_bp, url_prefix='/course-reviews')
+courseReviews_bp = Blueprint(
+    name='courseReviews',
+    import_name=__name__,
+    root_path=rootPath,
+    template_folder=rootPath + "/front-end/src/blueprints/Course_Reviews/templates",
+    static_folder=rootPath + "/front-end/src/static"
+)
 
-db = SQLAlchemy(app)
+courseReviews_Flask = Flask('courseReviews')
+
+courseReviews_Flask.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reviews.db'
+courseReviews_Flask.config['SECRET_KEY'] = 'your_secret_key'
+
+db = SQLAlchemy(courseReviews_Flask)
 
 class Course(db.Model):
     id          = db.Column(db.Integer, primary_key=True)
@@ -138,26 +121,8 @@ class ReviewForm(FlaskForm):
     # Add other new form fields as needed...
 
 
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        print("Request Method = POST")
-    else:
-        print("Request Method = GET")
-    return render_template('index.html')
-
-
-@app.route('/course-registration')
-def courseRegistration():
-    return render_template('Registration/course_registration.html')
-
-@app.route('/course-search')
-def courseSearch():
-    return render_template('Course-Search/course_search.html')
-
-@app.route('/reviews', methods=['GET', 'POST'])
-def review():
+@courseReviews_bp.route('/course-review', methods=['GET', 'POST'])
+def courseReview():
     form = ReviewForm()
     if form.validate_on_submit():
         rating = form.professor_rate.data
@@ -165,7 +130,7 @@ def review():
         if not course:
             flash('Course not found! Please add the course before submitting a review.', 'error')
             return redirect(url_for('index'))
-        
+
         new_review = Review(
             course_id=course.id,
             review_text=form.review_text.data,
@@ -183,35 +148,34 @@ def review():
         db.session.commit()
         flash('Review added successfully!', 'success')
         return redirect(url_for('index'))
-    return render_template('Review/reviews.html', form=form)
+    return render_template('reviews.html', form=form)
 
-@app.route('/select_course')
-def select_course():
-    return render_template('Review/course_selection.html')
 
-@app.route('/culture')
-def culture():
-    return render_template('Cultural-Credits/culture.html')
-
-@app.route('/progress-tracker')
-def progressTracker():
+@courseReviews_bp.route('/course-selection', methods=['GET', 'POST'])
+def courseSelection():
     try:
-        courses_data = list()
+        courses = list()
 
         progress = mysql.connector.connect(
             host="174.138.53.254",
             user="TheAuditor",
-            password=password,
+            password=passwordTA,
             database="progress"
         )
         myProgress = progress.cursor(prepared=True)
-        sql_query = """SELECT `id`, `courseNumber`, `subject`, `courseReferenceNumber`, `courseTitle`, `campusCode`, `termDescription`, `gpaHours`, `hoursAttempted`, `hoursEarned`, `midtermGrade`, `finalGrade`, `qualityPoints` FROM agarofalo;"""
+        data = "qualityPoints"
+        sql_query = """SELECT courseTitle FROM `800737736` WHERE qualityPoints IS NOT NULL;"""
         myProgress.execute(sql_query)
         myResult = myProgress.fetchall()
 
         for x in myResult:
-            courses_data.append(x)
-        return render_template('APT/progress_tracker.html', courses=courses_data)
+            courses.append(x)
+            print(x)
+        if request.method == 'POST':
+            selected_course = request.form['course_name']
+            return render_template('reviews.html', course=selected_course)
+        else:
+            return render_template('course_selection.html', courses=courses)
 
     except mysql.connector.Error as error:
         print("query failed {}".format(error))
@@ -221,94 +185,83 @@ def progressTracker():
             myProgress.close()
             progress.close()
             print("MySQL connection is closed.")
-
-
-if __name__ == '__main__':
-    """
-    Must be placed at the end of the file.
-    """
-    # app.run(host = '192.168.1.46', port = 3000, debug = True)
-    #app.run(host = '127.0.0.1', port = 3000, debug = True)
-    app.run(host = '127.0.0.1', port = 9000, debug=True)
+    return render_template('course_selection.html', courses=courses)
 
 
 
 
-# # def allowed_file(filename):
-# #     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-# # ALLOWED_EXTENSIONS = {'csv'}
-# # class Index(Resource):
-#     # def get(self):
-#     #     data = []
-#     #     # render_template(webletIndex, data=data)
-#     #     return jsonify(render_template(webletIndex, data=data))
-#     # def post(self):
-#     #     data = request.get_json()
-#     #     return jsonify({'data': data}), 201
 
 
-# # @app.route("/index")
-# # def index():
-# #     return render_template(webletIndex)
 
 
-# # @app.route('/data')
-# # def database():
-# #     return 'data'
+# #COURSE REVIEWS
+# @views.route('/review')
+# def review():
+#     return render_template("Review/course_selection.html")
 
+# @views.route('/courses')
+# def courses():
+#     courses = Course.query.all()
+#     return render_template('Review/reviews.html', courses=courses)
 
-# # @app.route('/', methods=['GET', 'POST'])
-# # def index():
-# #     # data = []  # A list to hold our CSV data
+# @views.route('/all_reviews')
+# def all_reviews():
+#     reviews = Review.query.all()
+#     return render_template('Review/all_reviews.html', reviews=reviews)
 
-# #     # # Use raw string notation for the path or double up the backslashes
-# #     # userCSV_path = rootPath + '\\front-end\\src\\user.csv'
-# #     # reqsCSV_path = rootPath + r'\\reqs.csv'
-# #     # testCSV_path = rootPath + r'\\test.csv'
+# @views.route('/select_course')
+# def select_course():
+#     return render_template('Review/course_selection.html')
 
-# #     if request.method == 'POST':
-# #         email = request.form.get('student_email', None)  # Fetch the major entered by the user
+# @views.route('/review_form', methods=['POST'])
+# def review_form():
+#     selected_course = request.form['course_name']
+#     # Do something with the selected course, like render a new template
+#     return render_template('Review/review_form.html', course=selected_course)
 
-# #     #     # Read the predefined CSV file and store the data
-# #     #     #with open(userCSV_path, 'r') as f:
-# #     #      #   reader = csv.reader(f)
-# #     #       #  data = [row for row in reader]
+# @views.route('/submit_review', methods=['POST'])
+# def submit_review():
+#     form = ReviewForm(request.form)
+#     if form.validate_on_submit():
+#         try:
+#             # Assuming you have logic to get or handle the course_id appropriately
+#             course_id = course_selection.html  # Replace with actual logic to handle course_id
 
-# #     #     # Filter the data based on the major if one is entered
-# #     #     #if email:
-# #     #         # Assuming the major is in the 2nd column (index 1). Adjust the index if needed.
-# #     #      #   data = [data[0]] + [row for row in data[1:] if email.lower() in row[1].lower()]
-# #     else:
-# #         print("else Statement has been reached for /api/index.")
-# #     #     # print("GET method has been used for /api/index .")
+#             new_review = Review(
+#                 course_id=course_id,
+#                 review_text=form.review_text.data,
+#                 # ... other fields ...
+#             )
+#             db.session.add(new_review)
+#             db.session.commit()
 
-# #     return render_template("index.html")
-# #     # return render_template("index.html", data=data)
+#             # Save the review to a file
+#             with open("reviews.txt", "a") as file:
+#                 file.write(f"Course ID: {new_review.course_id}\n")
+#                 file.write(f"Review Text: {new_review.review_text}\n")
+#                 file.write("------\n")
 
+#             flash('Review submitted successfully!', 'success')
+#         except Exception as e:
+#             # If an error occurs, log it and flash a message
+#             flash(f'An error occurred: {str(e)}', 'error')
 
-# # @app.route('/api/data')
-# # def get_data():
-# #     data = {'message': 'Hello from the Flask API!'}
-# #     return jsonify(data)
+#         return redirect(url_for('index'))
 
+#     # If the form is not valid or it's a GET request, render the form again
+#     return render_template('reviews.html', form=form)
 
-# # Sydney's log
-#     # csv_path = rootPath + r'\\Academic Programs - Stetson University.csv'
-#     #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Databasefinal.csv'
-#     #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\DegreeAudit.Database.csv'
-#     #r'C:\Users\sydne\OneDrive - Stetson University, Inc\Desktop\Computer Science\Python\Academic_Programs_-_Stetson_University.csv'
+# @views.route('/download_reviews')
+# def download_reviews():
+#     directory = os.path.dirname(os.path.abspath(__file__))
+#     return send_from_directory(directory, "reviews.txt", as_attachment=True)
 
-#     # if request.method == 'POST':
-#     #     major = request.form.get('major', None)  # Fetch the major entered by the user
-
-#     #     # Read the predefined CSV file and store the data
-#     #     with open(csv_path, 'r') as f:
-#     #         reader = csv.reader(f)
-#     #         data = [row for row in reader]
-
-#     #     # Filter the data based on the major if one is entered
-#     #     if major:
-#     #         # Assuming the major is in the 2nd column (index 1). Adjust the index if needed.
-#     #         data = [data[0]] + [row for row in data[1:] if major.lower() in row[1].lower()]
-
-#     # return render_template(webletIndex, data=data)
+# @views.route('/reviews/<int:course_id>')
+# def show_reviews(course_id):
+#     course = Course.query.get(course_id)
+#     if course is None:
+#         flash('Course not found!', 'error')
+#         return redirect(url_for('index'))
+    
+#     reviews = Review.query.filter_by(course_id=course_id).all()
+#     return render_template('course_reviews.html', course=course, reviews=reviews)
